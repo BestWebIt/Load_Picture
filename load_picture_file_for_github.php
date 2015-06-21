495121bdb8ad27455ff3e34949c3ea9270e20ebc
Anatolij <akkauntforbest@gmail.com>
2015-06-21 19:00:00 +0300

    delete article page and add new page for github

---
+++ load_picture_file_for_github.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Зразок кодування для реалізації завантаження зображень на сервер" />
+	<TITLE>Завантаження зображень на сервер </TITLE>        
+</head>
+
+<body >
+<div style="width:600px;border: 1px solid #cdcdcd;padding:10px" >
+<h4>навчальний матеріал</h4>
+<em>червень 2015</em><br>
+<em>Автор: Анатолій</em>
+<h2>Завантаження зображень на сервер</h2>	
+<p>Зразок тестової робочої html-форми</p>
+
+<form action="http://bestwebit.biz.ua/pages_02/load_picture_file_for_github_exe.php"  method="post"  enctype="multipart/form-data" style="width:90%;background-color:#FFFACD;border:#cdcdcd border: 1px solid;padding:10px">
+   <p>Вкажіть малюнок в форматі JPEG, PNG чи GIF</p>
+   <p><input type="file" name="userfile" accept="image/jpeg,image/png,image/gif">
+   <input type="submit" value="Відправити"></p>
+</form>
+
+</div>
+</body>
+</html>
